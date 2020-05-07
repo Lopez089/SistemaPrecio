3a5a7c1c5719af5010d05b45b81a1fe7ffe34e56
Juan <aragonlopezjuan87@gmail.com>
2020-05-07 15:40:41 +0200

    mejoras CardFooterService

--- src/components/cardFooterService/index.js
+++ src/components/cardFooterService/index.js
@@ -3,9 +3,9 @@ import React from "react";
 const CardFooterService = (props) => {
   const { info, button } = props;
   return (
-    <React.Fragment>
+    <>
       <div className="mt-4 text-center">
-        <a href="/#" className="card-link">
+        <a href={info.url} className="card-link">
           {info.text}
         </a>
       </div>
@@ -13,12 +13,12 @@ const CardFooterService = (props) => {
         data-toggle="modal"
         data-target="#exampleModal"
         type="button"
-        className="rounded shadow btn btn-primary mt-4"
+        className="rounded shadow btn btn-primary mt-4 w-100"
       >
         {button.text}
         <i className="fas fa-angle-right ml-2"></i>
       </button>
-    </React.Fragment>
+    </>
   );
 };
 
